20236d0b0b19411611e8bd22e19c8d7029a3d9a1
korn-dev <kasikorntripforwork@gmail.com>
2025-11-06 02:04:11 +0700

    INIT : install shadcn components and set up supabase via drizzle

---
+++ src/server/db/schemas/posts.ts
@@ -0,0 +1,16 @@
+import { createTable } from "../lib/utils";
+import { index } from "drizzle-orm/pg-core";
+
+export const posts = createTable(
+  "post",
+  (d) => ({
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    name: d.varchar({ length: 256 }),
+    createdAt: d
+      .timestamp({ withTimezone: true })
+      .$defaultFn(() => /* @__PURE__ */ new Date())
+      .notNull(),
+    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  }),
+  (t) => [index("name_idx").on(t.name)]
+);
