1a5de163f98768689e7345b6ae29d2412045a10a
korn-dev <kasikorntripforwork@gmail.com>
2025-12-15 23:13:34 +0700

    FEAT : update shadcn v3.5 to 3.6.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,13 @@ import "@/styles/globals.css"
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next"
-import { Geist } from "next/font/google"
+import { Geist, Inter } from "next/font/google"
 import { ThemeProvider } from "next-themes"
 import { ErrorBoundary } from "@/components/custom/error-boundary"
 import { Toaster } from "@/components/ui/sonner"
 import { TRPCReactProvider } from "@/trpc/react"
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
 	title: "Kurio",
@@ -23,7 +25,7 @@ export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html className={geist.variable} lang="en" suppressHydrationWarning>
+		<html className={inter.variable} lang="en" suppressHydrationWarning>
 			<body>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 					<ErrorBoundary>
